4021a4f5cf3a1edc54a1ca548cc806ffc5898bff
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-11 11:34:35 +0100

    create function that check if the user is logged in and return the user info

--- assets/functions.js
+++ assets/functions.js
@@ -1,6 +1,6 @@
 // login operation
 function login(email, password) {
-    return new Promise((resolve, reject)=> {
+    return new Promise((resolve, reject) => {
         (async () => {
             // get the users info
             let res = await fetch("./assets/users.json");
@@ -10,7 +10,7 @@ function login(email, password) {
 
             let user = data.find(user => user.email == email);
 
-            if(user && user.password == password) {
+            if (user && user.password == password) {
                 sessionStorage.setItem("user", JSON.stringify(user));
                 resolve("logged in successfuly");
 
@@ -20,4 +20,11 @@ function login(email, password) {
 
         })();
     });
+}
+
+// check if the user is logged in and return the user info
+function userLogin() {
+    let user = sessionStorage.getItem("user");
+    if(user) return JSON.parse(user);
+    else return false
 }
