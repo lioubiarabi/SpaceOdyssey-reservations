a9334030584c6d70f11658b011f33c088bc3aa30
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-11 10:04:58 +0100

    check if the user is exist in json file

--- assets/functions.js
+++ assets/functions.js
@@ -1,6 +1,16 @@
 // login operation
 function login(email, password) {
     return new Promise((resolve, reject)=> {
-        
+        (async () => {
+            // get the users info
+            let res = await fetch("./assets/users.json");
+            let data = await res.json();
+
+            // check the users info
+
+            let user = data.find(user => user.email == email);
+            
+
+        })();
     });
 }
