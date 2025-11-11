818e02eab5fd60af8adea99593443ff92600822d
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-11 11:18:44 +0100

    link the login promise with the html page and the user can login

--- assets/login.js
+++ assets/login.js
@@ -1,2 +1,29 @@
-let email = document.getElementById("email").value,
-    password = document.getElementById("password").value;
+
+let loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (e) => {
+    
+    e.preventDefault();
+
+    let email = document.getElementById("email").value,
+        password = document.getElementById("password").value;
+
+    login(email, password).then(res => {
+        Notiflix.Report.success(
+            'Success',
+            res,
+            'Okay',
+        );
+        //redirection after login
+        setTimeout(() => {
+            window.open("./index.html", "_self");
+        }, 3000);
+    }).catch(error => {
+        Notiflix.Report.failure(
+            'Failure',
+            error,
+            'Okay',
+        );
+    });
+});
+
