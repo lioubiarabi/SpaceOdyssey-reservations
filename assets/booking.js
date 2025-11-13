7bcb4cdae86574fbb96039e88deae4f79450a26a
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-13 14:33:07 +0100

    rendering the destinations elements

--- assets/booking.js
+++ assets/booking.js
@@ -1,16 +1,25 @@
-// get destinations
-(async ()=>{
-    //get destinations
-    let destinationData = await fetch("./assets/destinations.json");
-    let destinationArray = await destinationData.json();
-
-    console.log(destinationArray)
-
-})()
-
 let booking = [];
 let destination = document.getElementById("destination");
 
-destination.addEventListener("change", ()=>{
+// get destinations
+(async () => {
+    //get destinations
+    try {
+        let destinationData = await fetch("./assets/destinations.json");
+        let destinationArray = await destinationData.json();
+
+        //render the destinations in select destination
+        destinationArray.forEach((dest, index) => {
+            destination.innerHTML += `<option value="${index}">${dest.name}</option>`;
+        });
+
+    } catch (error) {
+        console.log(error)
+    }
+})()
+
+
+
+destination.addEventListener("change", () => {
 
 });
